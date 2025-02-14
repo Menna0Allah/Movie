6b1734208e35d5057f2ddae2a760828696183e21
menna <menna.allah04@gmail.com>
2025-02-15 01:03:04 +0200

    convert the loading... to spinner that much better + making it true to see it

--- RV/src/App.jsx
+++ RV/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import Search from "./components/search"
+import Spinner from "./components/Spinner"
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -18,7 +19,7 @@ const App = () => {
 
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchMovies = async () => {
 
@@ -53,7 +54,7 @@ const App = () => {
 
     } finally {
 
-      setIsLoading(false);
+      setIsLoading(true);
 
     }
   }
@@ -80,7 +81,7 @@ const App = () => {
           <h2>All Movies</h2>
 
           {isLoading? (
-            <p className="text-white">Loading...</p>
+            <Spinner />
           ) : errorMessage? (
             <p className="text-red-500">{errorMessage}</p>
           ) :(
