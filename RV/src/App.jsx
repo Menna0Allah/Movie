041634ad2cd8d35394bd1d4005dfcd7f943679fe
menna <menna.allah04@gmail.com>
2025-02-14 16:51:36 +0200

    start to git

---
+++ RV/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import Search from "./components/search"
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          
+          <img src="./hero.png" alt="hero banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You will Enjoy Withod the Hassle</h1>
+
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  )
+}
+
+export default App
