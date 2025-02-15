2651b773f78ed6717d900e50a410936bb7ee5649
menna <menna.allah04@gmail.com>
2025-02-15 02:29:16 +0200

    Implement Search

--- RV/src/App.jsx
+++ RV/src/App.jsx
@@ -22,14 +22,16 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
 
-    setIsLoading(false);
+    setIsLoading(true);
     setErrorMessage('');
 
     try{
 
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -62,8 +64,9 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  },[])
+    fetchMovies(searchTerm);
+  },[searchTerm])
+  // so every time searchterm change it will change the variable immediately and also change query too 
 
   return (
     <main>
